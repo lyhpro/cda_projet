3449091ce61d13399bc7823a7f75fcf9286533aa
lyhpro <lyheunphaly.pro@gmail.com>
2024-06-20 20:27:31 +0200

    debut implementation formulaire recording

--- gestion_personnel_front/src/app/components/recording/recording.component.ts
+++ gestion_personnel_front/src/app/components/recording/recording.component.ts
@@ -1,18 +1,25 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee/employee/employee';
 import { Observable, Subscription } from 'rxjs';
 import { UserService } from '../../services/user/user.service';
 import { DayType } from '../../models/dayType/day-type';
+import { Recording } from '../../models/recording/recording';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recording',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './recording.component.html',
   styleUrl: './recording.component.css'
 })
 export class RecordingComponent implements OnInit {
+
+  recording!: Recording;
+  recordingForm!: FormGroup;
+
+  initTime!: Time;
 
   employees!: Employee[];
   employees$: Observable<Employee[]> | undefined;
@@ -23,12 +30,35 @@ export class RecordingComponent implements OnInit {
   subscriptiondayTypes!: Subscription;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    // this.initTime.hours = 0;
+    // this.initTime.minutes = 0;
+    this.recording = new Recording(0,new Date(),this.initTime, this.initTime, this.initTime,this.initTime,this.initTime,this.initTime,this.initTime,0,0);
+    this.recordingForm = this.formBuilder.group(
+      {
+        id: new FormControl(''),
+        date: new FormControl(''),
+        hourStart: new FormControl(''),
+        hourStop: new FormControl(''), 
+        breakStart: new FormControl(''),
+        breakStop: new FormControl(''),
+        totalHours: new FormControl(''), 
+        extraHours: new FormControl(''), 
+        dueHours: new FormControl(''), 
+        employeeId: new FormControl(''),
+        dayTypeId: new FormControl('')
+      }
+    )
     this.initEmployees();
     this.initDayTypes();
+  }
+
+  onSubmit() {
+
   }
 
   initEmployees() {
@@ -37,6 +67,7 @@ export class RecordingComponent implements OnInit {
       {
         next: resp => {
           this.employees = resp;
+          console.log(this.employees)
         },
         error: err => {
           console.log(err);
--- gestion_personnel_front/src/app/models/employee/employee/employee.ts
+++ gestion_personnel_front/src/app/models/employee/employee/employee.ts
@@ -1,2 +1,33 @@
 export class Employee {
+    id: number;
+    secondname: string;
+    firstname: string;
+    placeOfBirth: string;
+    dateOfBirth: Date;
+    enable: boolean;
+    dateOfCreation: Date;
+    contactDetailId: number;
+    professionalDetailId: number;
+
+    constructor(
+        id: number,
+        secondname: string,
+        firstname: string,
+        placeOfBirth: string,
+        dateOfBirth: Date,
+        enable: boolean,
+        dateOfCreation: Date,
+        contactDetailId: number,
+        professionalDetailId: number
+    ) {
+        this.id = id;
+        this.secondname = secondname;
+        this.firstname = firstname;
+        this.placeOfBirth = placeOfBirth;
+        this.dateOfBirth = dateOfBirth;
+        this.enable = enable;
+        this.dateOfCreation = dateOfCreation;
+        this.contactDetailId = contactDetailId;
+        this.professionalDetailId = professionalDetailId;
+    }
 }
