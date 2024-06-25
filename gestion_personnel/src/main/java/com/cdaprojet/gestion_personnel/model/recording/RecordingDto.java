ad13b9039f2fd25e083ba11638db73fdd3f1bf7f
lyhpro <lyheunphaly.pro@gmail.com>
2024-06-25 09:40:08 +0200

    modifi de recordingdto

--- gestion_personnel/src/main/java/com/cdaprojet/gestion_personnel/model/recording/RecordingDto.java
+++ gestion_personnel/src/main/java/com/cdaprojet/gestion_personnel/model/recording/RecordingDto.java
@@ -1,5 +1,6 @@
 package com.cdaprojet.gestion_personnel.model.recording;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -20,6 +21,9 @@ public class RecordingDto {
     private LocalTime hourStop;
     private LocalTime breakStart;
     private LocalTime breakStop;
+    private Duration extraHours;
+    private Duration dueHours;
+    private Duration totalHours;
     private long employeeId;
     private long dayTypeId;
 
@@ -30,6 +34,9 @@ public class RecordingDto {
         this.hourStop = recording.getHourStop();
         this.breakStart = recording.getBreakStart();
         this.breakStop = recording.getBreakStop();
+        this.extraHours = recording.getExtraHours();
+        this.dueHours = recording.getDueHours();
+        this.totalHours = recording.getTotalHours();
         this.employeeId = recording.getEmployee().getId();
         this.dayTypeId = recording.getDayType().getId();
     }
