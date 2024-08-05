5c73b1c7ce07da8e45d2ab4948e5b3b5b87a201b
lyhpro <lyheunphaly.pro@gmail.com>
2024-08-05 16:06:00 +0200

    <update>: modif route api email

--- gestion_personnel/src/main/java/com/cdaprojet/gestion_personnel/controller/EmailController.java
+++ gestion_personnel/src/main/java/com/cdaprojet/gestion_personnel/controller/EmailController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +23,9 @@ public class EmailController {
         return ResponseEntity.ok("Email d'activation du profil utilisateur envoyé");
     }
 
-    @GetMapping("/sendUpdatePasswordUserEmail/{token}")
-    public ResponseEntity<String> sendCreatedPwdUserEmail(@PathVariable String token) {
-        emailService.sendCreatedPwdUserEmail(token);
+    @GetMapping("/sendUpdatePasswordUserEmail")
+    public ResponseEntity<String> sendCreatedPwdUserEmail(String userFullname, String userEmail, long tokenId) {
+        emailService.sendCreatedPwdUserEmail(userFullname, userEmail, tokenId);
         return ResponseEntity.ok("Email de création de mot de passe utilisateur envoyé");
     }
 }
